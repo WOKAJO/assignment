a67145a045604b201693aa01ea6bf5a470842d7e
WOKAJO <fatane40@gmail.com>
2026-08-18 22:05:47 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "zizo-resume-v1";
+const CACHE_NAME = "zizo-cv-cache-v1";
 
 const FILES_TO_CACHE = [
     "index.html",
@@ -11,16 +11,46 @@ const FILES_TO_CACHE = [
     
 ];
 
-self.addEventListener("install", event => {
+// Install
+self.addEventListener("install", (event) => {
+    self.skipWaiting();
+
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
     );
 });
 
-self.addEventListener("fetch", event => {
+// Activate
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) =>
+            Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            )
+        )
+    );
+
+    self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
+        caches.match(event.request).then((cachedResponse) => {
+            return (
+                cachedResponse ||
+                fetch(event.request).catch(() => {
+                    if (event.request.mode === "navigate") {
+                        return caches.match("index.html");
+                    }
+                })
+            );
+        })
     );
 });
